516f547afe4da49620a091f7872873f6ff14b3d7
juangod1 <juan.godfrid@gmail.com>
2016-05-25 15:18:32 -0300

    front back headers etc

---
+++ blobsBack.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "blobsBack.h"
+#define BOARD_SIZE_Y 10
+#define BOARD_SIZE_X 10
+#define CLEAR_GRAPHICS for(i=0;i<50;i++) putchar('\n')
+
+int display_board(char board[BOARD_SIZE_X][BOARD_SIZE_Y])
+{
+	int i,j;
+
+	CLEAR_GRAPHICS;
+
+	for (i=0;i<BOARD_SIZE_Y;i++)
+	{
+		putchar('|');
+		for (j=0;j<BOARD_SIZE_X;j++)
+		{
+			printf("%c |",board[i][j]);
+		}
+		putchar('\n');
+	}
+
+}
+
+main(){
+
+}
+
+
+
+
---
+++ makefile
@@ -0,0 +1,4 @@
+ALL:
+	gcc -o -lm Trabajo_Final_PI blobsFront.c blobsBack.c
+clean:
+	rm Trabajo_Final_PI
