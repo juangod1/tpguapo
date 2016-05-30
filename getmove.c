3c64f702c69b25fe1c025146f4c9fd0afbbefc7f
pabloito <pradnic@itba.edu.ar>
2016-05-30 12:09:43 -0300

    getmove.c updateado

--- getmove.c
+++ getmove.c
@@ -1,24 +1,22 @@
-void getmove(int upnext, char board[BOARD_SIZE_MAX_Y][BOARD_SIZE_MAX_X])
+int getmove(int upnext, char board[BOARD_SIZE_MAX_Y][BOARD_SIZE_MAX_X])
 {
-	int jugadacorrecta=0;
+    int cuantosleyo=0, jugada=0;
 	display_board(board);
 	printf("Jugador %d escriba su próxima jugada\n",upnext);
-	while(!jugadacorrecta)
-	{
-		from_x=-1, from_y=-1, to_x=-1, to_y=-1
-		scanf("[%d,%d][%d,%d]", &from_x, &from_y, &to_x, &to_y);
-		if(getchar()!='\n'|| from_x==-1 || from_y==-1 || to_x==-1 || to_y==-1)
-		{
-			BORRA_BUFFER;
-			display_board(board);
-			printf("Error: Lectura de parámetros incorrectos. Intente nuevamente\n");
-		}
-		else
-		{
-			jugadacorrecta=check_move(from_x, from_y, to_x, to_y, board, upnext))
-			if(jugadacorrecta)
-				display_board(board);
-				printf("Error: Jugada Imposible. Intente nuevamente\n");
-		}
-	}
+    while(!jugada)
+    {
+        while((cuantosleyo=scanf("[%d,%d] [%d,%d]", &from_x, &from_y, &to_x, &to_y))!=4 || getchar()!='\n')
+        {
+            BORRA_BUFFER;
+            display_board(board);
+            printf("Error: Lectura de parámetros incorrectos. Intente nuevamente\nJugador %d escriba su próxima jugada\n", upnext);
+        }
+        jugada=check_move(from_x, from_y, to_x, to_y, board, upnext);
+        if(!jugada)
+        {
+            display_board(board);
+		    printf("Error: Jugada Imposible. Intente nuevamente\nJugador %d escriba su próxima jugada\n",upnext);
+        }
+    }
+    return jugada;
 }
