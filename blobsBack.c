053122618592f2b364924dee5f5fdeb000e6266c
frandelgado <delga96@gmail.com>
2016-06-04 14:43:21 -0300

    savefile y openfile

--- blobsBack.c
+++ blobsBack.c
@@ -161,3 +161,38 @@ int getmove_ai(char board[][BOARD_SIZE_MAX_X])
 	
 	return move_type;
 }
+
+int openfile(char *filename, int mode,int upnext,int size_y, int size_x,char board[30][30])
+{
+	/* Necesito declarar todos los parametros de entrada? o por ser golbales no es necesario? */
+	FILE * savefile;
+	savefile = fopen( filename, "r");
+	if(savefile==NULL){
+		printf("error en la lectura del archivo");
+		return 1;
+	}
+	fread(&mode ,sizeof(int),1, savefile);
+	fread(&upnext ,sizeof(int),1, savefile);								
+	fread(&size_y ,sizeof(int),1, savefile);
+	fread(&size_x ,sizeof(int),1, savefile);
+	fread(board ,sizeof(char),900, savefile);
+	fclose (savefile);
+	return 0;
+}
+int savefile(char *filename, int mode,int upnext,int size_y, int size_x,char board[30][30])
+{
+	/* Necesito declarar todos los parametros de entrada? o por ser golbales no es necesario? */
+	FILE * savefile;
+	savefile = fopen( filename, "w");
+	if(savefile==NULL){
+		printf("no se pudo crear el archivo");
+		return 1;
+	}
+	fwrite(&mode ,sizeof(int),1, savefile);
+	fwrite(&upnext ,sizeof(int),1, savefile);
+	fwrite(&size_y ,sizeof(int),1, savefile);
+	fwrite(&size_x ,sizeof(int),1, savefile);
+	fwrite(board ,sizeof(char),900, savefile);
+	fclose (savefile);
+	return 0;
+}
